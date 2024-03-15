c7477996bc044db57df5d4efefab031507fb1636
alina513 <130853290+alina513@users.noreply.github.com>
2024-03-16 00:06:04 +0200

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,4 @@
-import { selectCartItems } from '../../redux/selectors';
-import { useSelector } from 'react-redux';
+
 import { useDispatch } from 'react-redux';
 import {sendItemsToBackend} from "../../redux/operations";
 import css from "./Button.module.css"
